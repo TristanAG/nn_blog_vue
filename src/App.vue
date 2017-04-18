dcaf7fb06dc025786d012d3a93fc74afe5066c3c
Tristan Gruener <tgruener@nordicnaturals.com>
2017-04-18 09:34:34 -0700

    the most basic post ever

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <test></test>
+
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Test from './components/Test'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 40px;
+}
+
+img{
+  width: 33px;
+}
+</style>
