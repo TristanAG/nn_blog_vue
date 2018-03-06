53306e9b7814012e31bf672df8dbfe1d1dc894eb
Tristan Gruener <tristan.gruener@gmail.com>
2018-03-05 16:45:20 -0800

    forgot to update route

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -73,7 +73,8 @@ var webpackConfig = merge(baseWebpackConfig, {
         '/blog/home-life/five-healthy-essentials-to-pack-for-your-holiday-travels',
         '/blog/home-life/five-ways-to-keep-your-spirits-bright-after-the-holidays-end',
         '/blog/home-life/creating-healthy-habits-that-stick',
-        '/blog/home-life/how-relationships-affect-your-health'
+        '/blog/home-life/how-relationships-affect-your-health',
+        '/blog/home-life/how-to-read-a-supplement-label'
       ],
       {
         ignoreJSErrors: true
